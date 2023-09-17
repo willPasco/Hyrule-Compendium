efb929fb26b5fe497e62f0dd265a5e39c60bfeee
Willpasco <will.pasco.az@gmail.com>
2023-09-17 13:43:29 -0300

    Adding build type file on app base applications file.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,18 +10,6 @@ apply {
     from("$rootDir/buildSrc/app_base_application.kts")
 }
 
-android{
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile(Config.PROGUARD_OPTIMIZE_FILE),
-                Config.PROGUARD_RULES_FILE
-            )
-        }
-    }
-}
-
 dependencies {
 
     implementation(Dependencies.Android.ANDROIDX_CORE)
--- buildSrc/app_base_application.kts
+++ buildSrc/app_base_application.kts
@@ -1,6 +1,6 @@
 apply {
     from("$rootDir/buildSrc/app_default_config.kts")
-    from("$rootDir/buildSrc/build_compile_options.kts")
+    from("$rootDir/buildSrc/build_types.kts")
     from("$rootDir/buildSrc/compile_options.kts")
     from("$rootDir/buildSrc/kotlin_options.kts")
     from("$rootDir/buildSrc/dependencies_instrumented_tests.kts")
