53edce7458c6692132a165b395945ba0e3449fbb
Willpasco <will.pasco.az@gmail.com>
2023-09-17 19:33:46 -0300

    Adding design system module in data list module.

--- data-list/build.gradle.kts
+++ data-list/build.gradle.kts
@@ -7,7 +7,6 @@ apply {
     from("$rootDir/buildSrc/module_base_application.kts")
 }
 
-
 android {
     namespace = "com.will.data_list"
 }
@@ -16,6 +15,7 @@ dependencies {
     implementation(config.Dependencies.Android.ANDROIDX_CORE)
     implementation(config.Dependencies.Android.ANDROIDX_APP_COMPAT)
     implementation(config.Dependencies.Android.ANDROID_MATERIAL)
+    implementation(project(mapOf("path" to config.Dependencies.Modules.DESIGN_SYSTEM)))
     implementation(project(mapOf("path" to config.Dependencies.Modules.CORE)))
     implementation(project(mapOf("path" to config.Dependencies.Modules.COMMONS)))
 }
