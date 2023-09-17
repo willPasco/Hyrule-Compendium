abdd721f6f1c8d108aea4bf5f43b2e9326cbdecf
Willpasco <will.pasco.az@gmail.com>
2023-09-17 18:32:15 -0300

    Creating the data list module.

---
+++ data-list/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+}
+
+apply {
+    from("$rootDir/buildSrc/module_base_application.kts")
+}
+
+
+android {
+    namespace = "com.will.data_list"
+}
+
+dependencies {
+    implementation(config.Dependencies.Android.ANDROIDX_CORE)
+    implementation(config.Dependencies.Android.ANDROIDX_APP_COMPAT)
+    implementation(config.Dependencies.Android.ANDROID_MATERIAL)
+    implementation(project(mapOf("path" to config.Dependencies.Modules.CORE)))
+    implementation(project(mapOf("path" to config.Dependencies.Modules.COMMONS)))
+}
