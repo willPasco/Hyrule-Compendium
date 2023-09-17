f86ff727473f6356eeebddc18b4147764700661e
Willpasco <will.pasco.az@gmail.com>
2023-09-17 19:35:07 -0300

    Creating the main activity in app module.

---
+++ app/src/main/java/com/will/hyrulecompendium/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.will.hyrulecompendium
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.hyrule_compendium.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
