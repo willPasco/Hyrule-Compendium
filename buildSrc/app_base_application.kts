c0da0efd92e90ebf936dd4b6a9f9ab76cd586ffb
Willpasco <will.pasco.az@gmail.com>
2023-09-17 13:43:15 -0300

    Refactoring the name of gradle files.

--- buildSrc/app_base_application.kts
+++ buildSrc/app_base_application.kts
@@ -1,7 +1,8 @@
 apply {
     from("$rootDir/buildSrc/app_default_config.kts")
     from("$rootDir/buildSrc/build_compile_options.kts")
-    from("$rootDir/buildSrc/build_kotlin_options.kts")
+    from("$rootDir/buildSrc/compile_options.kts")
+    from("$rootDir/buildSrc/kotlin_options.kts")
     from("$rootDir/buildSrc/dependencies_instrumented_tests.kts")
     from("$rootDir/buildSrc/dependencies_unit_tests.kts")
 }
