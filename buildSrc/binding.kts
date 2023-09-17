eeca6a42ca48f8ca7abdf8db997be6b0e2067324
Willpasco <will.pasco.az@gmail.com>
2023-09-17 19:07:26 -0300

    Fixing wrong configuration in bindings file.

--- buildSrc/binding.kts
+++ buildSrc/binding.kts
@@ -1,12 +1,3 @@
-apply {
-    from("$rootDir/buildSrc/app_default_config.kts")
-    from("$rootDir/buildSrc/build_types.kts")
-    from("$rootDir/buildSrc/compile_options.kts")
-    from("$rootDir/buildSrc/kotlin_options.kts")
-    from("$rootDir/buildSrc/dependencies_instrumented_tests.kts")
-    from("$rootDir/buildSrc/dependencies_unit_tests.kts")
-}
-
 android {
     viewBinding {
         enabled = true
