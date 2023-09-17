e30e39b97d588d5e03b318d82900a508266984a7
Willpasco <will.pasco.az@gmail.com>
2023-09-17 13:44:28 -0300

    Using the dependencies reference from kts files.

--- design-system/build.gradle.kts
+++ design-system/build.gradle.kts
@@ -13,7 +13,7 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
+    implementation(config.Dependencies.Android.ANDROIDX_CORE)
+    implementation(config.Dependencies.Android.ANDROIDX_APP_COMPAT)
+    implementation(config.Dependencies.Android.ANDROID_MATERIAL)
 }
