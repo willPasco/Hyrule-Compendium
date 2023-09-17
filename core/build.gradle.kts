d3ecdd9371e71be7217ba7a7f5b830e88c726ab5
Willpasco <will.pasco.az@gmail.com>
2023-09-17 16:18:45 -0300

    Creating the core module.

---
+++ core/build.gradle.kts
@@ -0,0 +1,19 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+}
+
+apply {
+    from("$rootDir/buildSrc/module_base_application.kts")
+}
+
+android {
+    namespace = "com.will.core"
+}
+
+dependencies {
+
+    implementation(config.Dependencies.Android.ANDROIDX_CORE)
+    implementation(config.Dependencies.Android.ANDROIDX_APP_COMPAT)
+    implementation(config.Dependencies.Android.ANDROID_MATERIAL)
+}
